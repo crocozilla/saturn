cf82097baac8df5cf58ede5b434d1f3a0eb44bc7
Renzo Rossato Battisti <renzo.battisti1@gmail.com>
2024-10-05 13:33:38 -0300

    apparently done

--- linker/linker_test.go
+++ linker/linker_test.go
@@ -7,4 +7,7 @@ import (
 
 func TestRun(t *testing.T) {
 	_ = Run(assembler.Run("linker_test.asm", "linker_test_part2.asm"))
+	_ = Run(assembler.Run("linker_test_3.asm", "linker_test_3.asm"))
+	// todo: compare first run with MAIN_test goal
+	// and second run with TESTE3_test goal
 }
